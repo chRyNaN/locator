bae7eb6e275ca8073f79360a462c96dd2638f0b2
Chris <ckeenan@starry.com>
2019-08-13 14:08:15 -0400

    Added additional attach and detach functions outside the LocatesWith interface scope

--- src/commonMain/kotlin/com/chrynan/locator/LocatesWith.kt
+++ src/commonMain/kotlin/com/chrynan/locator/LocatesWith.kt
@@ -9,6 +9,10 @@ inline fun <reified M : Module> LocatesWith<M>.attachToDependencyGraph() = Depen
 
 inline fun <reified M : Module> LocatesWith<M>.detachFromDependencyGraph() = DependencyGraph.detachModule<M>()
 
+inline fun <reified M : Module> attachToDependencyGraph(module: M) = DependencyGraph.attachModule(module)
+
+inline fun <reified M : Module> detachFromDependencyGraph() = DependencyGraph.detachModule<M>()
+
 inline fun <reified M : Module, R> LocatesWith<M>.locate(noinline moduleAccessor: M.() -> R) =
     Locator(moduleAccessor = moduleAccessor, module = module)
 
